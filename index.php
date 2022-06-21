0738b530e6b56e19419057904fcac019760ae189
Guilherme Coelho <coelho.guilhermedacruz@gmail.com>
2022-06-21 07:23:19 -0300

    metodos lista

--- index.php
+++ index.php
@@ -4,14 +4,42 @@ require_once("config.php");
 
 /*$sql = new sql();
 
-$clientes = $sql->select("SELECT * FROM tb_clientes");
+$Usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-echo json_encode($clientes);*/
+echo json_encode($usuarios);*/
 
-$Guilherme = new Clientes();
 
-$Guilherme->loadById(1);
+// EX: 01
 
-echo $Guilherme;
+/*carrega um usuario
+
+$Gui = new Usuarios();
+
+$Gui->loadById(1);
+
+echo $Gui;*/
+
+
+// EX: 02
+
+/*carrega uma lista de usuarios
+
+$list = Clientes::getList();
+
+echo json_encode($list);*/
+
+// EX: 03
+
+//carrega uma lista de usuarios buscando pelo login
+
+//$search = Clientes::search("G");
+//echo json_encode($search);
+
+//EX: 04 carrega um usuario usando login e senha
+
+$usuario = new Usuarios();
+$usuario->login("Gui", "0944");
+
+echo $usuario;
 
 ?>

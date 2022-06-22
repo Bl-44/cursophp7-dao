13e32d8200f21910d02fe0e1f5cda75a33675534
Guilherme Coelho <coelho.guilhermedacruz@gmail.com>
2022-06-22 08:00:02 -0300

    inserir e alterar dados

--- index.php
+++ index.php
@@ -6,14 +6,14 @@ require_once("config.php");
 
 $Usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-echo json_encode($usuarios);*/
+echo json_encode($usuario);*/
 
 
 // EX: 01
 
 /*carrega um usuario
 
-$Gui = new Usuarios();
+$Gui = new Usuario();
 
 $Gui->loadById(1);
 
@@ -37,8 +37,24 @@ echo json_encode($list);*/
 
 //EX: 04 carrega um usuario usando login e senha
 
-$usuario = new Usuarios();
-$usuario->login("Gui", "0944");
+//$usuario = new Usuario();
+//$usuario->login("Gui", "0944");
+
+//echo $usuario;
+
+//EX:05 cria um novo usuario
+
+//$Jordan = new Usuario("jordan", "9007");
+
+//$Jordan->insert();
+
+//echo $Jordan;
+
+//EX:06 alterando usuario UPDATE
+
+$usuario = new Usuario();
+$usuario->loadById(7);
+$usuario->update("Lebron", "2015");
 
 echo $usuario;
 
